1124316e140e5e33cd8514d64d07d915c012e527
gizmore <gizmore@wechall.net>
2024-12-30 22:40:42 +0100

    'sync'

--- method/view.py
+++ method/view.py
@@ -1,6 +1,7 @@
 from gdo.base.GDT import GDT
 from gdo.base.Method import Method
 from gdo.core.GDT_Object import GDT_Object
+from gdo.date.Time import Time
 from gdo.pm.GDO_PM import GDO_PM
 from gdo.ui.GDT_Card import GDT_Card
 
@@ -20,6 +21,8 @@ class view(Method):
 
     def gdo_execute(self) -> GDT:
         pm = self.get_pm()
+        if pm.gdo_val('pm ')is_unread():
+        pm.save_val('pm_read', Time.get_date())
         card = GDT_Card()
         card.add_field(*pm.table().columns_only('pm_from', 'pm_to,', 'pm_title', 'pm_message'))
         return card
